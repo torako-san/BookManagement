d6b07bd22ef97b9d017b4a9c4a8f8ffc6d492768
torako-san <100730549+torako-san@users.noreply.github.com>
2022-10-28 10:08:56 +0900

    画面表示用のメソッドを追記

--- src/main/java/com/example/demo/domain/model/Book.java
+++ src/main/java/com/example/demo/domain/model/Book.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain.model;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.example.demo.controller.form.BookForm;
@@ -37,4 +38,26 @@ public class Book {
 	private String deleted_user;
 	private Date deleted_at;
 	private int version;
+	
+	// ISBNコードをハイフンで区切って表示
+	public String isbnFormat() {
+		String prefix = isbn.substring(0,3);			// 接頭記号(3桁)
+		String group = isbn.substring(3,4);				// グループ記号(1桁)
+		String publisher = isbn.substring(4,10);		// 出版者記号(6桁)
+		String bookName = isbn.substring(10,12);		// 書名記号(2桁)
+		String checkDigit = isbn.substring(12,13);		// チェックディジット(1桁)
+		return String.join("-", prefix, group, publisher, bookName, checkDigit);
+	}
+
+	// ゼロを3つごとにカンマで区切る
+	public String priceCommaOf1000() {
+		return String.format("%,d", price);
+	}
+
+	// 日付を和暦(年月日)表示にする
+	public String publicationDateOfYyyymmdd() {
+		if(publication_date == null) return "";
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+		return sdf.format(publication_date).toString();
+	}
 }
