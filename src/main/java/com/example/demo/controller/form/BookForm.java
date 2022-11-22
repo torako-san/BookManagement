bb43fc6c9fadf2d6da2ee92be9cfbbbb9cb90c75
torako-san <100730549+torako-san@users.noreply.github.com>
2022-11-22 11:31:32 +0900

    記述の修正

--- src/main/java/com/example/demo/controller/form/BookForm.java
+++ src/main/java/com/example/demo/controller/form/BookForm.java
@@ -9,7 +9,7 @@ public class BookForm {
 	private int id;
 	private String name;
 	private String isbn;
-	private String decription;
+	private String description;
 	private String publisher;
 	private Date publication_date;
 	private int version;
