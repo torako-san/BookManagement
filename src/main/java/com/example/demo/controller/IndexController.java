7bcd83787e3f58f9e0f04062623b7d26d1204142
torako-san <100730549+torako-san@users.noreply.github.com>
2022-11-24 13:49:26 +0900

    書籍データ登録する処理を記述

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -2,22 +2,46 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import com.example.demo.controller.form.BookForm;
+import com.example.demo.BookRepository;
+import com.example.demo.model.Book;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 	
+	private final BookRepository repository;
+	
 	@GetMapping("/")
-	public String getIndex() {
+	public String index() {
 		 return "index";
 	}
 	
 	@GetMapping("book/add")
-	public String getAdd(BookForm form, Model model) {
-		model.addAttribute("bookForm", form);
+	public String add(@ModelAttribute Book book) {
 		return "book/add";
+	}
+	
+	@PostMapping("book/list")
+	public String list(@Validated @ModelAttribute Book book, BindingResult result) {
+		if (result.hasErrors()) {
+			return "book/add";
+		}
+		repository.save(book);
+		return "redirect:/";
+	}
+
+	@GetMapping("book/list")
+	public String showList (Model model){
+		model.addAttribute("books", repository.findAll());
+		return "book/list";
 	}
 	
 }
